09657bc8611e0b085e1a496551acfa5d96f559bd
Sheehan-zou <zys_amassing@hotmail.com>
2022-12-10 18:23:41 +0800

    modify something 3

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharms')
+    print_hi('PyCharms3')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
